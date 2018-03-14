25acf75085b0af362719caf3b47c8a76b00e47d7
Samuli Ristimäki <samuli.ristimaki@gmail.com>
2018-03-14 22:15:57 +0200

    Setup draw options

--- components/Chromata.vue
+++ components/Chromata.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container">
-    <img src="test.jpg" id="image" class="my-image">
+    <img src="unnamed.jpg" id="image" class="my-image">
     <script src="chromata.min.js"></script>
     <script>
       var image = document.querySelector('#image');
       var chromata = new Chromata(image, {
-          pathFinderCount: 60,
-          speed: 9,
-          turningAngle: Math.PI/2,
-          colorMode: 'grescale',
-          lineWidth: 4,
-          lineMode: 'square',
-          compositeOperation: 'saturation',
-          origin: ['50% 50%'],
+          pathFinderCount: 250, // 1 - 500
+          speed: 8, // 1 - 20
+          turningAngle: Math.PI/2, // Math.PI
+          colorMode: 'greyscale', // color, greyscale
+          lineWidth: 3, // 1-10
+          lineMode: 'square', // smooth, square, point
+          compositeOperation: 'lighten', // lighten, saturation
+          origin: ['50% 50%'], // bottom, top, left, right
           outputSize: 'container', // original, container
-          key: 'high',
+          key: 'low', // low, high
           backgroundColor: 'hsla(34, 70%, 70%, 0)'
       });
       chromata.start();
